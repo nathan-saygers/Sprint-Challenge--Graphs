725d60d0b19c30d83bfb0c7a08224da0af72daff
nathan-saygers <nathan.saygers@gmail.com>
2020-05-14 14:43:36 -0700

    add some notes from beej

---
+++ additional_notes.py
@@ -0,0 +1,13 @@
+"""
+Strategies when reaching dead end:
+    - bfs to find ?s
+    - keep track of current path and back track checking each room for ?s
+    - recursion does this automatically (but you need to track your backtracks)
+
+DFT and tracking backtrack should be enough to get MVP
+
+runtime is irrelevant
+
+
+
+"""
