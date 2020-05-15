268e0ae067eee11c0b71d77ca01135b5ed7d8689
nathan-saygers <nathan.saygers@gmail.com>
2020-05-15 09:38:57 -0700

    added origin to player

--- player.py
+++ player.py
@@ -1,9 +1,13 @@
 class Player:
     def __init__(self, starting_room):
         self.current_room = starting_room
-    def travel(self, direction, show_rooms = False):
+        self.origin = ''
+
+    def travel(self, direction, show_rooms=False):
         next_room = self.current_room.get_room_in_direction(direction)
         if next_room is not None:
+            print(
+                f"IM MOVING from {self.current_room.name} to {next_room.name}")
             self.current_room = next_room
             if (show_rooms):
                 next_room.print_room_description(self)
